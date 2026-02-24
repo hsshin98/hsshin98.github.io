7b99ad2cb34ba3143d2141f91c842bb3b3edf72c
hsshin98 <37581790+hsshin98@users.noreply.github.com>
2026-02-24 15:15:12 +0000

    Deploying to gh-pages from @ hsshin98/hsshin98.github.io@eb91d0c2e7d1e75f1c359b83027fd3c5b139bbf9 🚀

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,1 +1,1 @@
-$(document).ready((function(){medium_zoom=mediumZoom("[data-zoomable]",{background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"})}));
+$(document).ready((function(){medium_zoom=mediumZoom("[data-zoomable]",{background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"}),$(".profile figure img").on("click",(function(){$(this).closest(".profile").find(".profile-letterbox-wrapper").slideToggle(200)}))}));
